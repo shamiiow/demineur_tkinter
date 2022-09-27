db50deecb06151b80084b679f6ecbc037ae9d205
shamiiow <ishihlanzo@gmail.com>
2022-09-27 16:43:50 +0200

    Commit

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ import random
 
 root = tkinter.Tk()
 root.geometry("500x500")
-LONG = 10
-NB_BOMB = 10
+LONG = 25
+NB_BOMB = 20
 grille = [[0 for j in range(LONG)] for i in range(LONG)]
 
 for i in range(NB_BOMB):
@@ -17,12 +17,87 @@ for i in range(NB_BOMB):
             grille[cx][cy] = 9
             break
 
+for i in range(1, LONG-1):
+    for j in range(1, LONG-1):
+        if grille[i][j] != 9:
+            if grille[i][j-1] == 9:
+                grille[i][j] += 1
+            if grille[i][j+1] == 9:
+                grille[i][j] += 1
 
-for i in range(LONG) :
+            for k in range(3):
+                if grille[i+1][j-(k-1)] == 9:
+                    grille[i][j] += 1
+            for k in range(3):
+                if grille[i-1][j-(k-1)] == 9:
+                    grille[i][j] += 1
+
+# border
+# grille[0]
+for i in range(1, LONG-1):
+    if grille[0][i] != 9:
+        if grille[0][i+1] == 9:
+            grille[0][i] += 1
+        if grille[0][i-1] == 9:
+            grille[0][i] += 1
+
+        if grille[1][i+1] == 9:
+            grille[0][i] += 1
+        if grille[1][i-1] == 9:
+            grille[0][i] += 1
+        if grille[1][i] == 9:
+            grille[0][i] += 1
+
+# grille[LONG]
+for i in range(1, LONG-1):
+    if grille[LONG-1][i] != 9:
+        if grille[LONG-1][i+1] == 9:
+            grille[LONG-1][i] += 1
+        if grille[LONG-1][i-1] == 9:
+            grille[LONG-1][i] += 1
+
+        if grille[LONG-2][i+1] == 9:
+            grille[LONG-1][i] += 1
+        if grille[LONG-2][i-1] == 9:
+            grille[LONG-1][i] += 1
+        if grille[LONG-2][i] == 9:
+            grille[LONG-1][i] += 1
+
+# grille left
+for i in range(1, LONG-1):
+    if grille[i][0] != 9:
+        if grille[i+1][0] == 9:
+            grille[i][0] += 1
+        if grille[i-1][0] == 9:
+            grille[i][0] += 1
+
+        if grille[i+1][1] == 9:
+            grille[i][0] += 1
+        if grille[i-1][1] == 9:
+            grille[i][0] += 1
+        if grille[i][1] == 9:
+            grille[i][0] += 1
+
+# grille right
+for i in range(1, LONG-1):
+    if grille[i][LONG-1] != 9:
+        if grille[i+1][LONG-1] == 9:
+            grille[i][LONG-1] += 1
+        if grille[i-1][LONG-1] == 9:
+            grille[i][LONG-1] += 1
+
+        if grille[i+1][LONG-2] == 9:
+            grille[i][LONG-1] += 1
+        if grille[i-1][LONG-2] == 9:
+            grille[i][LONG-1] += 1
+        if grille[i][LONG-2] == 9:
+            grille[i][LONG-1] += 1
+
+for i in range(LONG):
     print(grille[i])
 
 img = {}
-for i in range(10): #temporaire
+for i in range(10):
     img[i] = tkinter.PhotoImage(file=f"img/{i}.png")
 
 
@@ -33,13 +108,13 @@ def userplay(coord):
 answer = {}
 for i in range(LONG):
     for j in range(LONG):
-        answer[(i, j)] = tkinter.Label(root, image=img[grille[i][j]]) #add that when all img are create : ""
+        answer[(i, j)] = tkinter.Label(root, image=img[grille[i][j]])
         answer[(i, j)].grid(row=i, column=j, ipadx=1, ipady=3)
 
 button = {}
 for i in range(LONG):
     for j in range(LONG):
         button[(i, j)] = tkinter.Button(root, command=lambda x=(i, j): userplay(x))
-        button[(i, j)].grid(row=i, column=j, ipadx=6)
+        #button[(i, j)].grid(row=i, column=j, ipadx=6)
 
 root.mainloop()
