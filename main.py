00795cab83b0eae7aa0a1f40b6bdfb124aedaef1
shamiiow <ishihlanzo@gmail.com>
2022-09-28 21:24:57 +0200

    a rajouter au jeu : -la découverte des case vides (elle est seulement parcielle dans le code actuelle) -la victoire -les drapeau a rajouter au GUI : -timer -nombre de bombe restante sois disant non marquer -replay button

--- main.py
+++ main.py
@@ -7,22 +7,35 @@ import fonction
 root = tkinter.Tk()
 root.geometry("500x500")
 LONG = 10
-NB_BOMB = 5
+NB_BOMB = 1
 grille = [[0 for j in range(LONG)] for i in range(LONG)]
 
 grille = fonction.bomb(NB_BOMB, grille)
 grille = fonction.number_fill(LONG, grille)
 
 
-def user_click(coord):
+
+def user_click(coord, nb_bomb):
+    if (len(button)) == nb_bomb:
+        result.config(text="baka..")
     button[coord].grid_forget()
+    if grille[coord[0]][coord[1]] == 9:
+        for i in range(LONG):
+            for j in range(LONG):
+                button[(i, j)].grid_forget()
+        result.config(text="L.O.O.S.E.R")
     if 0 < coord[0] < (LONG-1) and 0 < coord[1] < (LONG-1):
         if grille[coord[0]][coord[1]] == 0:
             for i in range(0, 3, 2):
                 if grille[coord[0]-(i-1)][coord[1]] != 9:
                     button[(coord[0]-(i-1), coord[1])].grid_forget()
+
                 if grille[coord[0]][coord[1]-(i-1)] != 9:
                     button[(coord[0], coord[1]-(i-1))].grid_forget()
+
+    print((len(button)))
+    print(nb_bomb)
+    print("-------------")
 
 
 def put_flag(event):
@@ -37,13 +50,16 @@ answer = {}
 for i in range(LONG):
     for j in range(LONG):
         answer[(i, j)] = tkinter.Label(root, image=img[grille[i][j]])
-        answer[(i, j)].grid(row=i, column=j, ipadx=2, ipady=3)
+        answer[(i, j)].grid(row=i+1, column=j, ipadx=2, ipady=3, sticky="N")
 
 button = {}
 for i in range(LONG):
     for j in range(LONG):
-        button[(i, j)] = tkinter.Button(root, command=lambda x=(i, j): user_click(x))
-        button[(i, j)].grid(row=i, column=j, ipadx=6)
+        button[(i, j)] = tkinter.Button(root, command=lambda x=(i, j), y=NB_BOMB: user_click(x,y))
+        button[(i, j)].grid(row=i+1, column=j, ipadx=6)
         button[(i, j)].bind("<Button-3>", lambda x="test": put_flag(x))
+
+result = tkinter.Label(root, font=("Minercaft", 20, "bold"))
+result.grid(row=0, columnspan=LONG)
 
 root.mainloop()
