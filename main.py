1c07aef9c425b9d3923ffe8f76640a687679df33
shamiiow <ishihlanzo@gmail.com>
2022-09-28 21:35:47 +0200

    a rajouter au jeu : -la découverte des case vides (elle est seulement parcielle dans le code actuelle) -la victoire -les drapeau a rajouter au GUI : -timer -nombre de bombe restante sois disant non marquer -replay button

--- main.py
+++ main.py
@@ -4,16 +4,26 @@
 import tkinter
 import fonction
 
+# create the windows
+
 root = tkinter.Tk()
 root.geometry("500x500")
+
+# create the constant and the game grid
+
 LONG = 10
 NB_BOMB = 1
 grille = [[0 for j in range(LONG)] for i in range(LONG)]
 
+# place the bomb and put the right number around them
+
 grille = fonction.bomb(NB_BOMB, grille)
 grille = fonction.number_fill(LONG, grille)
 
-def discovery(coord) :
+# this fonction is for cascading discovery of the boxes (not finish (because not working proprely))
+
+
+def discovery(coord):
     if 0 < coord[0] < (LONG-1) and 0 < coord[1] < (LONG-1):
         if grille[coord[0]][coord[1]] == 0:
             for i in range(0, 3, 2):
@@ -21,6 +31,8 @@ def discovery(coord) :
                     button[(coord[0]-(i-1), coord[1])].grid_forget()
                 if grille[coord[0]][coord[1]-(i-1)] != 9:
                     button[(coord[0], coord[1]-(i-1))].grid_forget()
+
+# this function is excecuted every time the player click
 
 
 def user_click(coord, nb_bomb):
@@ -34,18 +46,20 @@ def user_click(coord, nb_bomb):
         result.config(text="L.O.O.S.E.R")
     discovery(coord)
 
-    print((len(button)))
-    print(nb_bomb)
-    print("-------------")
+# this function are going to be for the flap, but it need to be implemented
 
 
 def put_flag(event):
     print(event)
 
+# loading the image for the game
+
 
 img = {}
 for i in range(10):
     img[i] = tkinter.PhotoImage(file=f"img/{i}.png")
+
+# create the 2 layers of widget for the game
 
 answer = {}
 for i in range(LONG):
@@ -56,11 +70,15 @@ for i in range(LONG):
 button = {}
 for i in range(LONG):
     for j in range(LONG):
-        button[(i, j)] = tkinter.Button(root, command=lambda x=(i, j), y=NB_BOMB: user_click(x,y))
+        button[(i, j)] = tkinter.Button(root, command=lambda x=(i, j), y=NB_BOMB: user_click(x, y))
         button[(i, j)].grid(row=i+1, column=j, ipadx=6)
         button[(i, j)].bind("<Button-3>", lambda x="test": put_flag(x))
 
+# widget for telling the player if he loose or win
+
 result = tkinter.Label(root, font=("Minercaft", 20, "bold"))
 result.grid(row=0, columnspan=LONG)
+
+# render the windows
 
 root.mainloop()
