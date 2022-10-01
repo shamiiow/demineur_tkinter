72f45720b470162fe6fb713f8aca5673a9a01707
shamiiow <ishihlanzo@gmail.com>
2022-10-01 21:18:52 +0200

    ajout : -pré condition -post condition
    ------------------------
    
    a rajouter au jeu :
    -les drapeau
    a rajouter au GUI :
    -timer
    -replay button

--- fonction.py
+++ fonction.py
@@ -1,20 +1,20 @@
 import random
 
 
-def number_fill(long, grille):
+def number_fill(long: int, grille: list) -> list:
     for i in range(1, long - 1):
         for j in range(1, long - 1):
             if grille[i][j] != 9:
                 for k in range(-1, 2):
-                    if grille[i][j+k] == 9:
-                        grille[i][j] += 1
                     for p in range(-1, 2, 2):
                         if grille[i+p][j-k] == 9:
                             grille[i][j] += 1
+                    if grille[i][j+k] == 9:
+                        grille[i][j] += 1
     return grille
 
 
-def bomb(nb_bomb, grille):
+def bomb(nb_bomb: int, grille: list) -> list:
     for i in range(nb_bomb):
         while True:
             cx = random.randint(0, len(grille)-1)
@@ -22,5 +22,4 @@ def bomb(nb_bomb, grille):
             if grille[cx][cy] == 0:
                 grille[cx][cy] = 9
                 break
-
     return grille
--- main.py
+++ main.py
@@ -5,11 +5,10 @@ import tkinter
 import fonction
 
 
-def game():
-    # create the constant and the game grid
+def game(LONG: int, NB_BOMB: int):
 
-    LONG = 15
-    NB_BOMB = 20
+    # set up the game
+
     grille = [[0 for i in range(LONG)] for i in range(LONG)]
     discovered = [[0 for j in range(LONG)] for i in range(LONG)]
 
@@ -20,71 +19,77 @@ def game():
 
     for i in range(LONG):
         grille[0][i] = 100
-        grille[LONG - 1][i] = 100
+        grille[LONG-1][i] = 100
         grille[i][0] = 100
-        grille[i][LONG - 1] = 100
+        grille[i][LONG-1] = 100
+
         discovered[0][i] = 100
-        discovered[LONG - 1][i] = 100
+        discovered[LONG-1][i] = 100
         discovered[i][0] = 100
-        discovered[i][LONG - 1] = 100
+        discovered[i][LONG-1] = 100
 
     # place the bomb and put the right number around them
 
     grille = fonction.bomb(NB_BOMB, grille)
     grille = fonction.number_fill(LONG, grille)
 
-    for i in range(LONG):
-        for j in range(LONG):
-            if grille[i][j] == 9:
-                discovered[i][j] = 5
-
     # this function is for cascading discovery of the boxes (not finish (because not working properly))
 
-    def discovery(coord):
+    def discovery(coord: tuple):
         if not (0 < coord[0] < len(grille) and 0 < coord[1] < len(grille)):
             return
-        for i in range(-1,2):
-            for j in range(-1,2,2):
+        for i in [-1, 0, 1]:
+            for j in [-1, 1]:
                 if (0 <= grille[coord[0]+j][coord[1]+i] < 9) and (discovered[coord[0]+j][coord[1]+i] == 0):
                     button[(coord[0]+j, coord[1]+i)].grid_forget()
                     discovered[coord[0]+j][coord[1]+i] = 1
                     if grille[coord[0]+j][coord[1]+i] == 0:
                         discovery((coord[0]+j, coord[1]+i))
-        for i in range(-1, 2, 2):
+        for i in [-1, 1]:
             if (0 <= grille[coord[0]][coord[1]+i] < 9) and (discovered[coord[0]][coord[1]+i] == 0):
                 button[(coord[0], coord[1]+i)].grid_forget()
                 discovered[coord[0]][coord[1]+i] = 1
                 if grille[coord[0]][coord[1]+i] == 0:
                     discovery((coord[0], coord[1]+i))
 
-    # this function is executed every time the player click
+    # check if the player win
 
-    def user_click(coord):
+    def win():
         x = 0
-        if grille[coord[0]][coord[1]] == 10:
-            return
-    #   if result.cget('text') != '':
-    #       return
-        button[coord].grid_forget()
-        if grille[coord[0]][coord[1]] == 9:
-            for i in range(LONG):
-                for j in range(LONG):
-                    if grille[i][j] == 9:
-                        button[(i, j)].grid_forget()
-            print('Tu as perdu')
-        if grille[coord[0]][coord[1]] == 0:
-            discovery(coord)
-        else:
-            discovered[coord[0]][coord[1]] = 1
         for i in range(LONG):
             if 0 not in discovered[i]:
                 x += 1
         if x == LONG:
             print('Tu as win')
 
+    # check if the player loose
+
+    def loose(coord: tuple):
+        if grille[coord[0]][coord[1]] == 9:
+            for i in range(LONG):
+                for j in range(LONG):
+                    if grille[i][j] == 9:
+                        button[(i, j)].grid_forget()
+            print('Tu as perdu')
+
+    # this function is executed every time the player click
+
+    def user_click(coord: tuple):
+        if grille[coord[0]][coord[1]] > 10:
+            return
+        button[coord].grid_forget()
+
+        if grille[coord[0]][coord[1]] == 0:
+            discovery(coord)
+        else:
+            discovered[coord[0]][coord[1]] = 1
+
+        loose(coord)
+        win()
+
     # this function are going to be for the flap, but it needs to be implemented
 
-    def put_flag(event, x):
+    def put_flag(event, x: int):
         print(x)
 
     # loading the image for the game
@@ -95,50 +100,68 @@ def game():
 
     # create the 2 layers of widget for the game
 
-    gamef = tkinter.Frame(root, bd=2, bg='red')
-    gamef.grid(row=1, column=0)
+    game_frame = tkinter.Frame(root, bd=2, bg='red')
+    game_frame.grid(row=1, column=0)
 
-    statf = tkinter.Frame(root, bd=2, bg='blue')
-    statf.grid(row=0, column=0)
+    counter_frame = tkinter.Frame(root, bd=2, bg='blue')
+    counter_frame.grid(row=0, column=0)
 
     answer = {}
     for i in range(1, LONG-1):
         for j in range(1, LONG-1):
-            answer[(i, j)] = tkinter.Label(gamef, image=img[grille[i][j]])
-            answer[(i, j)].grid(row=i+1, column=j, ipadx=3, ipady=3, sticky="N")
+            if grille[i][j] == 9:
+                discovered[i][j] = 5
+            answer[(i, j)] = tkinter.Label(game_frame, image=img[grille[i][j]])
+            answer[(i, j)].grid(row=i+1, column=j, ipadx=3, ipady=3, sticky="w")
 
     button = {}
     for i in range(1, LONG-1):
         for j in range(1, LONG-1):
-            button[(i, j)] = tkinter.Button(gamef, command=lambda x=(i, j): user_click(x))
+            button[(i, j)] = tkinter.Button(game_frame, command=lambda x=(i, j): user_click(x))
             button[(i, j)].grid(row=i+1, column=j, ipadx=7)
             button[(i, j)].bind("<Button-3>", lambda e, x=(i, j): put_flag(e, x))
 
-    bomb_r = tkinter.Label(statf, text=LONG, font=("Minecraft", 20, "bold"))
+    bomb_r = tkinter.Label(counter_frame, text=LONG, font=("Minecraft", 20, "bold"))
     bomb_r.grid(row=0, column=0)
 
-    restart = tkinter.Button(statf, text='Restart', command=game)
+    # this button doesn't work
+
+    restart = tkinter.Button(counter_frame, text='Restart', command=game)
     restart.grid(row=0, column=1)
+
     # render the windows
 
     root.mainloop()
 
 
-game()
+settings = []
 
-"""main = tkinter.Tk()
-main.geometry("500x100")
+main = tkinter.Tk()
+main.geometry("500x200")
+
+
+def play():
+    settings.append(int(len_grid.get()))
+    if (int(number_bomb.get())) > (int(len_grid.get()**2)):
+        print(int(len_grid.get()**2))
+        settings.append(len_grid.get() ** 2)
+    else:
+        settings.append(int(number_bomb.get()))
+    main.destroy()
+
 
 len_grid = tkinter.Scale(main, from_=30, to=0, orient=tkinter.VERTICAL)
 number_bomb = tkinter.Scale(main, from_=69, to=0, orient=tkinter.VERTICAL)
-abc = tkinter.Button(main)
-bcd = tkinter.Button(main)
+text_grid = tkinter.Label(main, text="  Longer of the  \ngrid")
+text_bomb = tkinter.Label(main, text="Number of\nbomb")
+launch = tkinter.Button(main, text='Launch\nthe game !', command=play)
 
-fg = tkinter.Button(main,text = 'je be large', command = Game )
-len_grid.place(x = 200)
-number_bomb.place(x = 200+50)
-abc.place(x= 0)
-fg.place(x=50)
+len_grid.place(x=175)
+text_grid.place(x=175-15, y=105)
+number_bomb.place(x=175+100)
+text_bomb.place(x=175+100-5, y=105)
+launch.place(x=220, y=150)
 
 main.mainloop()
-"""
+
+game(settings[0]+2, settings[1])
