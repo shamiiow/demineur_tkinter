a0b9bcd87204bca06c0e7c27687f0af5fad4dc09
ishihlanzo <ishihlanzo@gmail.com>
2022-10-04 22:56:06 +0200

    everything work all texture are here, i think it's the end everything is clean

--- main.py
+++ main.py
@@ -7,50 +7,49 @@ import tkinter
 import fonction
 
 
-def game(LONG: int, NB_BOMB: int, L=500, l=500) -> None:
+def game(long: int, nb_bomb: int, size_x=500, size_y=500) -> None:
 
     # set up the game
 
-    grille = [[0 for i in range(LONG)] for i in range(LONG)]
-    discovered = [[0 for j in range(LONG)] for i in range(LONG)]
-    drapeau = [[0 for j in range(LONG)] for i in range(LONG)]
+    grid = [[0 for i in range(long)] for i in range(long)]
+    discovered = [[0 for j in range(long)] for i in range(long)]
+    flag = [[0 for j in range(long)] for i in range(long)]
 
     # create the windows
 
     root = tkinter.Tk()
-    root.geometry(f"{L}x{l}+300+50")
-    root.title("Démineur")
+    root.geometry(f"{size_x}x{size_y}+300+50")
+    root.title("Minesweeper")
     root.iconbitmap('img/game_icon.ico')
     root.configure(bg='#bdbdbd')
 
     # create border of number around all the grid
 
-    for i in range(LONG):
-        grille[0][i] = 100
-        grille[LONG-1][i] = 100
-        grille[i][0] = 100
-        grille[i][LONG-1] = 100
+    for i in range(long):
+        grid[0][i] = 100
+        grid[long - 1][i] = 100
+        grid[i][0] = 100
+        grid[i][long - 1] = 100
         discovered[0][i] = 100
-        discovered[LONG-1][i] = 100
+        discovered[long - 1][i] = 100
         discovered[i][0] = 100
-        discovered[i][LONG-1] = 100
-        drapeau[0][i] = 100
-        drapeau[LONG-1][i] = 100
-        drapeau[i][0] = 100
-        drapeau[i][LONG-1] = 100
+        discovered[i][long - 1] = 100
+        flag[0][i] = 100
+        flag[long - 1][i] = 100
+        flag[i][0] = 100
+        flag[i][long - 1] = 100
 
     # place the bomb and put the right number around them
 
-    grille = fonction.bomb(NB_BOMB, grille)
-    grille = fonction.number_fill(LONG, grille)
+    grid = fonction.bomb(nb_bomb, grid)
+    grid = fonction.number_fill(long, grid)
 
     # This function is here only for debug
 
     def debug():
-        print('---------------------------------------------------------------------------------------------------------')
-        for i in range(1, LONG - 1):
-            print(grille[i], '--', discovered[i], '--', drapeau[i])
-
+        print('-------------------------------------------------------------------------')
+        for i in range(1, long - 1):
+            print(grid[i], '--', discovered[i], '--', flag[i])
 
     def dim(event) -> None:
         print("*********Size of the window*********")
@@ -65,65 +64,65 @@ def game(LONG: int, NB_BOMB: int, L=500, l=500) -> None:
 
     def discovery(coord: tuple) -> None:
         x, y = coord[0], coord[1]
-        if not ((0 < x < len(grille)) and (0 < y < len(grille))):
+        if not ((0 < x < len(grid)) and (0 < y < len(grid))):
             return
-        if drapeau[coord[0]][coord[1]] == 1:
+        if flag[coord[0]][coord[1]] == 1:
             return
         for i in [-1, 0, 1]:
             for j in [-1, 1]:
-                if (0 <= grille[x+j][y+i] < 9) and (discovered[x+j][y+i] == 0) and (drapeau[x+j][y+i] == 0):
+                if (0 <= grid[x+j][y+i] < 9) and (discovered[x+j][y+i] == 0) and (flag[x+j][y+i] == 0):
                     button[(x+j, y+i)].grid_forget()
                     discovered[x+j][y+i] = 1
-                    if grille[x+j][y+i] == 0 and (drapeau[x][y+i] == 0):
+                    if grid[x+j][y+i] == 0 and (flag[x][y+i] == 0):
                         discovery((x+j, y+i))
         for i in [-1, 1]:
-            if (0 <= grille[x][y+i] < 9) and (discovered[x][y+i] == 0) and (drapeau[x][y+i] == 0):
+            if (0 <= grid[x][y+i] < 9) and (discovered[x][y+i] == 0) and (flag[x][y+i] == 0):
                 button[(x, y+i)].grid_forget()
                 discovered[x][y+i] = 1
-                if grille[x][y+i] == 0 and (drapeau[x][y+i] == 0):
+                if grid[x][y+i] == 0 and (flag[x][y+i] == 0):
                     discovery((x, y+i))
 
     # check if the player win
 
     def win() -> None:
         x = 0
-        for i in range(LONG):
+        for i in range(long):
             if 0 not in discovered[i]:
                 x += 1
-        if x == LONG:
+        if x == long:
             restart.config(image=img[14])
             wol.config(text='win')
-
 
     # check if the player loose
 
     def loose(coord: tuple) -> None:
-        if grille[coord[0]][coord[1]] != 9:
+        if grid[coord[0]][coord[1]] != 9:
             return
         answer[coord].config(image=img[15])
-        for i in range(LONG):
-            for j in range(LONG):
-                if grille[i][j] == 9:
+        for i in range(long):
+            for j in range(long):
+                if grid[i][j] == 9:
                     button[(i, j)].grid_forget()
-                if grille[i][j] != 9 and drapeau[i][j] == 1:
+                if grid[i][j] != 9 and flag[i][j] == 1:
                     button[(i, j)].grid_forget()
                     answer[(i, j)].config(image=img[16])
         restart.config(image=img[13])
-        wol.config(text ='loose')
+        wol.config(text='loose')
 
     # this function is executed every time the player click
 
     def user_click(coord: tuple) -> None:
         if wol.cget("text") != '':
             return
-        if drapeau[coord[0]][coord[1]] == 1:
+        if flag[coord[0]][coord[1]] == 1:
             return
-        if grille[coord[0]][coord[1]] > 10:
+        if grid[coord[0]][coord[1]] > 10:
             return
 
         button[coord].grid_forget()
-        if grille[coord[0]][coord[1]] == 0:
+        if grid[coord[0]][coord[1]] == 0:
             discovery(coord)
+            discovered[coord[0]][coord[1]] = 1
         else:
             discovered[coord[0]][coord[1]] = 1
         debug()
@@ -133,19 +132,18 @@ def game(LONG: int, NB_BOMB: int, L=500, l=500) -> None:
     # this function are going to be for the flag, but it needs to be implemented
 
     def put_flag(event, coord: tuple) -> None:
-        if drapeau[coord[0]][coord[1]] == 0:
-            drapeau[coord[0]][coord[1]] = 1
+        if flag[coord[0]][coord[1]] == 0:
+            flag[coord[0]][coord[1]] = 1
             button[coord].grid_forget()
             button[coord].grid(row=coord[0]+1, column=coord[1])
             button[coord].config(image=img[10])
             bomb_r.config(text=bomb_r.cget('text')-1)
-        elif drapeau[coord[0]][coord[1]] == 1:
-            drapeau[coord[0]][coord[1]] = 0
+        elif flag[coord[0]][coord[1]] == 1:
+            flag[coord[0]][coord[1]] = 0
             button[coord].grid_forget()
-            button[coord].grid(row=coord[0]+1, column=coord[1], ipadx= 7)
+            button[coord].grid(row=coord[0]+1, column=coord[1], ipadx=7)
             button[coord].config(image='')
             bomb_r.config(text=bomb_r.cget('text')+1)
-
 
     def replay() -> None:
         root.destroy()
@@ -168,18 +166,18 @@ def game(LONG: int, NB_BOMB: int, L=500, l=500) -> None:
     # create the grid for the answer and grid it
 
     answer = {}
-    for i in range(1, LONG-1):
-        for j in range(1, LONG-1):
-            if grille[i][j] == 9:
+    for i in range(1, long - 1):
+        for j in range(1, long - 1):
+            if grid[i][j] == 9:
                 discovered[i][j] = 5
-            answer[(i, j)] = tkinter.Label(game_frame, image=img[grille[i][j]], bg="#C0ADAC")
+            answer[(i, j)] = tkinter.Label(game_frame, image=img[grid[i][j]], bg="#C0ADAC")
             answer[(i, j)].grid(row=i+1, column=j, ipadx=3, ipady=3, sticky="w")
 
     # create the button to hide the grid answer and grid it
 
     button = {}
-    for i in range(1, LONG-1):
-        for j in range(1, LONG-1):
+    for i in range(1, long - 1):
+        for j in range(1, long - 1):
             button[(i, j)] = tkinter.Button(game_frame, bg="#606060", command=lambda x=(i, j): user_click(x))
             button[(i, j)].grid(row=i+1, column=j, ipadx=7)
             button[(i, j)].bind("<Button-3>", lambda e, x=(i, j): put_flag(e, x))
@@ -187,13 +185,13 @@ def game(LONG: int, NB_BOMB: int, L=500, l=500) -> None:
 
     # show the number of bomb it last
 
-    bomb_r = tkinter.Label(counter_frame, text=NB_BOMB, bg='#bdbdbd', font=("Minecraft", 20, "bold"))
-    bomb_r.grid(row=0, column=0, padx=25, ipadx=25)
+    bomb_r = tkinter.Label(counter_frame, text=nb_bomb, bg='#bdbdbd', font=("Minecraft", 20, "bold"))
+    bomb_r.grid(row=0, column=0)
 
     # this is for restart the game
 
     restart = tkinter.Button(counter_frame, image=img[12], bg='#bdbdbd', command=replay)
-    restart.grid(row=0, column=1, padx=25)
+    restart.grid(row=0, column=1, padx=50)
 
     wol = tkinter.Label(root, text='')
 
@@ -229,7 +227,7 @@ def game_settings() -> None:
 
     # set default settings and launch the game
 
-    def debutant_play():
+    def easy():
         settings[0] = 247
         settings[1] = 257
         settings.append(7)
@@ -238,7 +236,7 @@ def game_settings() -> None:
 
     # set default settings and launch the game
 
-    def intermediaire_play():
+    def normal():
         settings[0] = 479
         settings[1] = 489
         settings.append(16)
@@ -247,7 +245,7 @@ def game_settings() -> None:
 
     # set default settings and launch the game
 
-    def expert_play():
+    def hard():
         settings[0] = 634
         settings[1] = 644
         settings.append(22)
@@ -257,11 +255,11 @@ def game_settings() -> None:
     # create all the widget I need
 
     default = tkinter.Label(main, text='The default settings :', font=("Minecraft", 11))
-    debutant = tkinter.Button(main, text='Debutant', font=("Minecraft", 11), command=debutant_play)
-    intermediaire = tkinter.Button(main, text='Intermediaire', font=("Minecraft", 11), command=intermediaire_play)
-    expert = tkinter.Button(main, text='Expert', font=("Minecraft", 11), command=expert_play)
+    easy = tkinter.Button(main, text='Easy', font=("Minecraft", 11), command=easy)
+    normal = tkinter.Button(main, text='Normal', font=("Minecraft", 11), command=normal)
+    hard = tkinter.Button(main, text='Hard', font=("Minecraft", 11), command=hard)
 
-    dorc = tkinter.Label(main, text='Or\nCustom\nThe\nSettings', font="Minecraft")
+    text = tkinter.Label(main, text='Or\nCustom\nThe\nSettings', font="Minecraft")
 
     len_grid = tkinter.Scale(main, from_=30, to=0, orient=tkinter.VERTICAL)
     number_bomb = tkinter.Scale(main, from_=150, to=0, orient=tkinter.VERTICAL)
@@ -274,11 +272,11 @@ def game_settings() -> None:
     # place all the widget (I know place method is not the best)
 
     default.place(x=25, y=20)
-    debutant.place(x=25, y=50)
-    intermediaire.place(x=25, y=80)
-    expert.place(x=25, y=110)
+    easy.place(x=25, y=50)
+    normal.place(x=25, y=80)
+    hard.place(x=25, y=110)
 
-    dorc.place(x=200, y=50)
+    text.place(x=200, y=50)
 
     len_grid.place(x=325)
     text_grid.place(x=325-15, y=105)
