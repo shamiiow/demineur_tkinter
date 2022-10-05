ab0505ebe2e3abf3c7f5c3917ba2beb24e2f8652
ishihlanzo <ishihlanzo@gmail.com>
2022-10-05 09:47:09 +0200

    commit

--- main.py
+++ main.py
@@ -197,7 +197,7 @@ def game(long: int, nb_bomb: int, size_x=500, size_y=500) -> None:
 
     wol = tkinter.Label(root, text='')
 
-    #so exit the game
+    # to exit the game
 
     exit = tkinter.Button(counter_frame, image=img[17], bg='#bdbdbd', command=root.destroy)
     exit.grid(row=0, column=2)
